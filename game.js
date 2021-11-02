3e9fae33fe57092369ebff75050a98e3db95990c
Alistair-B-HW <91805078+Alistair-B-HW@users.noreply.github.com>
2021-11-02 18:40:16 +0000

    Bear Move Finished Completed bear movement (Dynamic speed setting too)

--- game.js
+++ game.js
@@ -15,7 +15,19 @@ function Bear() {
   this.move = function (xDir, yDir) {
     this.x += this.dBear * xDir;
     this.y += this.dBear * yDir;
+    this.fitBounds();
     this.display();
+  };
+  this.fitBounds = function () {
+    let parent = this.htmlElement.parentElement;
+    let iw = this.htmlElement.offsetWidth;
+    let ih = this.htmlElement.offsetHeight;
+    let w = parent.offsetWidth;
+    let h = parent.offsetHeight;
+    if (this.x < 0) this.x = 0;
+    if (this.x > w - iw) this.x = w - iw;
+    if (this.y < 0) this.y = 0;
+    if (this.y > h - ih) this.y = h - ih;
   };
   this.display = function () {
     this.htmlElement.style.left = this.x + "px";
@@ -46,23 +58,6 @@ function moveBear(e) {
   } // down key
 }
 
-this.fitBounds = function () {
-  let parent = this.htmlElement.parentElement;
-  let iw = this.htmlElement.offsetWidth;
-  let ih = this.htmlElement.offsetHeight;
-  let l = parent.offsetLeft;
-  let t = parent.offsetTop;
-  let w = parent.offsetWidth;
-  let h = parent.offsetHeight;
-  if (this.x < 0) this.x = 0;
-  if (this.x > w - iw) this.x = w - iw;
-  if (this.y < 0) this.y = 0;
-  if (this.y > h - ih) this.y = h - ih;
-};
-
-this.move = function (xDir, yDir) {
-  this.fitBounds(); //we add this instruction to keep bear within board
-  this.x += this.dBear * xDir;
-  this.y += this.dBear * yDir;
-  this.display();
-};
+function setSpeed() {
+  this.dBear = document.getElementById("dBare").value;
+}
